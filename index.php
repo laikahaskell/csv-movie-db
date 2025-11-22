ce25ad1c95f843e91e4a1b95cc2313156a57cd8a
Alex <apyryt518@aol.com>
2025-11-22 13:54:42 -0500

    add php

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</head>
+
+
+
+<body>
+
+
+<!-- Main content table here -->
+<div class="mx-5">
+<table class="table">
+<thead>
+
+<tr>
+	<th scope="col">film</th>
+	<th scope="col">director</th>
+	<th scope="col">release</th>
+	<th scope="col">year</th>
+	<th scope="col">rating</th>
+</tr>
+
+</thead>
+
+<tbody>
+<?php
+
+$row = -1;
+if (($handle = fopen("movies.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+	    if($row != -1){
+		$num = count($data);
+		$row++;
+		$output = "<tr><th scope='row'>$row</th>";
+        	for ($c=0; $c < $num; $c++) {
+        		$output .= "<td>" . $data[$c] . "</td>";
+		}
+		$output .= "</tr>";	
+		echo $output;
+	    }else{$row++;}
+	    
+	}
+    fclose($handle);
+}
+?>
+</tbody>
+</div>
+
+
+</body>
+</html>
